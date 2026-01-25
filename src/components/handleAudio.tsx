6675e8249978a8e5c3845babaa9336b5b72fb6b7
mamata pandey <mamata@Mac.Home>
2026-01-25 16:46:26 +0000

    create audio media

--- src/components/handleAudio.tsx
+++ src/components/handleAudio.tsx
@@ -1,11 +1,21 @@
 import { useEffect, useRef } from "react";
 
 export const handleAudio = (type: string) => {
-  const audiRef = useRef(null);
+  const audioRef = useRef(null);
   const mediaRef = useRef<MediaSource>(null);
+  const bufferRef = useRef<any>(null);
 
   useEffect(() => {
     const media = new MediaSource();
     mediaRef.current = media;
+
+    const audio: any = audioRef.current;
+    audio.src = URL.createObjectURL(media);
+
+    media.addEventListener("sourceopen", () => {
+      bufferRef.current = media.addSourceBuffer(type);
+
+      bufferRef.current.addEventListener("updateend");
+    });
   }, []);
 };
